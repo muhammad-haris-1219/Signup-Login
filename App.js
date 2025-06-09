12dce526865968d7aadcdefd8b8b82c88b6963e7
muhammad-haris-1219 <engrharis19@gmail.com>
2025-06-09 19:24:56 +0500

    refined

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ let creatingElement = () => {
         type: 'password', name: 'Password', placeholder: 'Enter Your Password'
     });
     loginAdmin = Object.assign(document.createElement('input'), {
-        type: 'password', name: 'Password', placeholder: 'For only Admin'
+        type: 'password', name: 'Password', placeholder: 'For Only Admin'
     });
     container.classList.add('container');
     container.append(loginEmail, loginPassword, loginAdmin);
@@ -78,79 +78,3 @@ loginBtn.addEventListener('click', () => {
     });
 
 });
-
-
-
-
-
-
-
-// let loginEmail, loginPassword, loginAdmin;
-// let getConsumerData = localStorage.getItem("consumerData");
-// let users = getConsumerData ? JSON.parse(getConsumerData) : [];
-// let empty = false;
-// let displayed = true;
-// signBtn.addEventListener('click', () => {
-//     console.warn(empty)
-//     // let consumer = new user(
-//     //     dataFields[0].value, dataFields[1].value,
-//     //     dataFields[2].value, dataFields[3].value,
-//     //     dataFields[4].value
-//     // );
-//     let consumer = new user(...[...dataFields].map(field => field.value));
-//     let adminData = new admin(...[...dataFields].map(field => field.value), dataFields[5].value);
-//     for (let getfiled = 0; getfiled < [...[...dataFields]].length - 1; getfiled++) {
-//         if ([...[...dataFields]][getfiled].value != "") { empty = true };
-//     };
-//     if (empty) {
-//         users.push(adminData ? adminData : consumer);
-//         localStorage.setItem("consumerData", JSON.stringify(users));
-//         for (let i = 0; i < dataFields.length; i++) { dataFields[i].style.display = 'none'; displayed = false; };
-//         creatingElement();empty = false;
-//     }
-//     else { alert('Do Fill-up Form'); displayed = true; }
-
-
-//     // Fields of signup pages blanked
-//     [...[...dataFields].map(field => field.value = '')];
-// });
-
-// let switched = true;
-// loginBtn.addEventListener('click', () => {
-//     if (displayed) {
-//         for (let inputField of dataFields) {
-//             inputField.style.display = 'none';
-//         }
-//         creatingElement();
-//         displayed = false;
-//     } else {
-
-//         for (let j = 0; j < users.length; j++) {
-//             // console.log(loginEmail.value)
-//             if (loginEmail.value != "" && loginEmail.value == users[j].userEmailing && loginPassword.value == users[j].userPasswording) {
-//                 if (loginAdmin.value != "" && loginAdmin.value == users[j].adminCode) {
-//                     console.log('admin portal', users[j].adminCode)
-//                     switched = false;
-//                     break;
-//                 };
-//                 console.log('Registration Succesfully', users[j].userEmailing)
-//                 alert('Registration Succesfully')
-//                 // window.location.href = 'https://github.com/muhammad-haris-1219/Signup-Login';
-//                 switched = false;
-//                 break;
-//             };
-//         };
-//         if (switched) {
-//             alert('Do Signup Before Login')
-//             for (let i = 0; i < dataFields.length; i++) { dataFields[i].style.display = 'grid'; }
-//             loginEmail.remove(); loginPassword.remove(); loginAdmin.remove();
-//             container.classList.remove('container');
-//             switched = true;
-//         };
-//     };
-// });
-
-
-
-
-
